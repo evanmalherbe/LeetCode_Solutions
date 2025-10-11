338fbce2904d6cf52ce841c72a35e1a9cb4dd3b3
Evan <evan@ix.co.za>
2025-10-11 17:08:35 +0200

    Add project files.

---
+++ TwoSum/Challenge.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeApp
+{
+	public class Challenge
+	{
+    public int[] TwoSum(int[] nums, int target) 
+    {
+      //Input: nums = [3,2,4], target = 6
+      //Output: [1,2]
+      int outerIndex = 0;
+      int lengthOfArray = nums.Count();
+      List<int> actualSolution = new List<int>();
+      bool isSolved = false;
+			foreach (int number in nums) 
+      {
+        int innerIndex = 0;
+        foreach(int value in nums)
+        {
+          // Can't add the same number in array to itself
+          if (innerIndex == outerIndex)
+          {
+            continue;
+          }
+          bool solution = number + value == target;
+          if (solution) 
+          {
+            actualSolution = new List<int>()
+            {
+              innerIndex,
+              outerIndex
+            };
+            isSolved = true;
+            break;
+          }
+          innerIndex++;
+        }
+        if (isSolved)
+        {
+          break;
+        }
+        outerIndex++;
+      }
+
+      return new int[] { actualSolution[0], actualSolution[1] };
+    }
+	}
+}
---
+++ TwoSum/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeApp
+{
+	public class Program
+	{
+		static void Main(string[] args)
+		{
+			Challenge challenge = new Challenge();
+    
+			int[] output = challenge.TwoSum(new int[] {3,3}, 6);
+			Console.WriteLine($"The solution is: [{output[0]},{output[1]}]." );
+		}
+	}
+}
