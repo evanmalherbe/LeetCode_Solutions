cb93622d2b85a02d6e1b3182b8d8f670a593b55f
Evan <evan@ix.co.za>
2025-10-11 17:49:35 +0200

    Solved it with char arrays

--- PalindromeNumber/Program.cs
+++ PalindromeNumber/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Remoting.Messaging;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,7 +11,9 @@ namespace PalindromeNumber
 	{
 		static void Main(string[] args)
 		{
-
+			Solution solution = new Solution();
+			bool answer = solution.IsPalindrome(121);
+			Console.WriteLine(answer);
 		}
 	}
 }
---
+++ PalindromeNumber/Solution.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PalindromeNumber
+{
+	public class Solution
+	{
+		public bool IsPalindrome(int x)
+		{
+			// x = 121
+			// output = true
+			string initialStringOfX = x.ToString();
+			char[] charArray = initialStringOfX.ToCharArray();
+			if (charArray.Length == 1) 
+			{
+				return true;
+			}
+			List<char> reverseCharArray = new List<char>();
+			for (int i = charArray.Length - 1; i >= 0; i--) 
+			{
+				var t = charArray[i];
+				reverseCharArray.Add(charArray[i]);
+			}
+			string reversedString = string.Join("", reverseCharArray);
+
+			if (initialStringOfX == reversedString)
+			{
+				 return true;
+			}
+
+			return false;
+		}
+	}
+}
